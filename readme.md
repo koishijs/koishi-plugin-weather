7d95c50a1c96e4dc850766c0df249665b55fe293
Shigma <shigma10826@gmail.com>
2022-02-26 00:54:27 +0800

    add implementation

--- readme.md
+++ readme.md
@@ -2,4 +2,10 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-weather?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-weather)
 
-Get Weather Information in Koishi
+在 Koishi 中查询天气。
+
+## 指令：weather
+
+- 基本语法：`weather <longitude> <latitude>`
+- 选项列表：
+  - -p \<product>  晴天钟产品选择，默认为 civil
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,33 @@
-import { Context } from 'koishi'
+import { Context, Schema, segment } from 'koishi'
+
+const lang = 'zh-CN'
+const unit = 'metric'
+const products = ['astro', 'civil', 'civillight', 'meteo', 'two']
+
+export interface Config {}
 
 export const name = 'weather'
+export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.command('weather <longitude> <latitude>', '查询天气')
+    // typescript cannot infer type from string templates
+    .option('product', `-p <product:string>  晴天钟产品选择，可为 ${products.join(', ')}`, { fallback: 'civil' })
+    .action(async ({ options }, lon, lat) => {
+      if (!lon || !lat) return '请输入经纬度。'
+      const { product } = options
+      if (!products.includes(options.product)) {
+        return `不支持该产品，产品选择应为 ${products.join(', ')} 之一。`
+      }
+      try {
+        const data = await ctx.http.get<ArrayBuffer>(`http://www.7timer.info/bin/${product}.php`, {
+          params: { lon, lat, lang, unit },
+          responseType: 'arraybuffer',
+        })
+        return segment.image(data)
+      } catch (error) {
+        ctx.logger('tools').warn(error)
+        return '请求失败。'
+      }
+    })
 }
